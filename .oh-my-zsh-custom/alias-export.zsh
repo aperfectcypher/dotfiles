b316f66de996bde428e15815e196fdb2ba524945
tla <tla@bea.be>
2020-07-30 14:57:46 +0200

    Special case for peco server, packages seems to be too old

--- .oh-my-zsh-custom/alias-export.zsh
+++ .oh-my-zsh-custom/alias-export.zsh
@@ -1,6 +1,6 @@
 # Define custom aliases and exports
 
-alias em='emacsclient -cs instance1'
+alias em='emacsclient -c'
 alias emacs='emacs -nw'
 alias gitl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
 export BLEEDING=/var/www/toaster/poky/build-toaster-2/tmp/deploy/images/bea-peco-mk1/bea-image-beast-bea-peco-mk1.ext4
